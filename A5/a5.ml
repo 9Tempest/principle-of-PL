6fa5a6cf8602dd2e05cb51957c272767e96f35ab
9Tempest <z277zhu@uwaterloo.ca>
2022-03-30 13:03:14 -0400

    complete A5

--- A5/a5.ml
+++ A5/a5.ml
@@ -4,6 +4,7 @@ open Core
 type storeVal =
     | NumVal of int
     | ProcDecl of procedure
+    | ArrayVal of int array
 
 let rec write store str (value: storeVal) =
     match store with
@@ -21,15 +22,22 @@ let rec get store str =
         else get rest str
     ) 
 
-let rec eval (exp: expr) store: (int) = 
+let extractNum (exp: storeVal): int = 
     match exp with
-    | Num num -> num
-    | AddExpr (e1, e2) -> (eval e1 store) + (eval e2 store)
-    | MulExpr (e1, e2) -> (eval e1 store) * (eval e2 store)
-    | NegExpr exp -> - (eval exp store)
-    | Var var -> match get store var with
-            | NumVal n -> n
-            | _ -> 0 
+    | NumVal n -> n
+    | _ -> 0
+
+let rec eval (exp: expr) store: storeVal = 
+    match exp with
+    | Num num -> NumVal num
+    | AddExpr (e1, e2) -> (NumVal ((extractNum (eval e1 store)) + (extractNum (eval e2 store))))
+    | MulExpr (e1, e2) -> (NumVal ((extractNum (eval e1 store)) * (extractNum (eval e2 store))))
+    | NegExpr exp ->  (NumVal (- extractNum (eval exp store)))
+    | Var var -> get store var 
+    | ArrIndexExpr (e1, e2) -> (
+        match eval e1 store with
+        | ArrayVal a -> NumVal(Array.get a (extractNum (eval e2 store)))
+    )
 
 let rec eval_bool (exp: boolExpr) store: (bool) = 
     match exp with
@@ -38,20 +46,16 @@ let rec eval_bool (exp: boolExpr) store: (bool) =
     | Not exp -> not (eval_bool exp store)
     | And (exp1, exp2) -> (eval_bool exp1 store) && (eval_bool exp2 store)
     | Or (exp1, exp2) -> (eval_bool exp1 store) || (eval_bool exp2 store)
-    | Gt (exp1, exp2) -> (eval exp1 store) > (eval exp2 store)
-    | Lt (exp1, exp2) -> (eval exp1 store) < (eval exp2 store)
-    | Eq (exp1, exp2) -> (eval exp1 store) == (eval exp2 store)
+    | Gt (exp1, exp2) -> (extractNum (eval exp1 store)) > (extractNum(eval exp2 store))
+    | Lt (exp1, exp2) -> (extractNum (eval exp1 store)) < (extractNum(eval exp2 store))
+    | Eq (exp1, exp2) -> (extractNum (eval exp1 store)) == (extractNum(eval exp2 store))
 
-let rec evalArgs (args: expr list) store = 
-    match args with
-    | [] -> []
-    | h::t -> (eval h store) :: (evalArgs t store)
 
-let rec assignArgs (args: int list) (exprs: string list) : (statement list) =
+let rec assignArgs (args: expr list) (exprs: string list) : (statement list) =
     match args with
     | [] -> []
     | arg:: resta -> match exprs with
-        | exp:: reste -> VarAssgStmt(exp, (Num(arg))) :: (assignArgs resta reste)
+        | exp:: reste -> VarAssgStmt(exp, arg) :: (assignArgs resta reste)
 
 let expandWhile expr stmt = IfStmt (expr, Block([stmt; WhileStmt (expr, stmt)]),Skip)
 
@@ -62,9 +66,9 @@ let rec runMain (stmts: statement list) store =
         | Block b -> (
             runMain b store
         )
-        | VarAssgStmt (target, expr) ->  write store target (NumVal (eval expr store)) 
+        | VarAssgStmt (target, expr) ->  write store target (eval expr store)
         | PrintStmt expr -> (
-            printf "%d\n" (eval expr store);
+            printf "%d\n" (extractNum (eval expr store));
             store
         )
         | IfStmt (exp, stmt1, stmt2) -> (
@@ -80,8 +84,20 @@ let rec runMain (stmts: statement list) store =
             match get store name with
             | ProcDecl (procName, params, decls, pstmts) -> (
                 let (params', pstmts') = (freshenProcedure params decls pstmts) in
-                let newArgs = (assignArgs (evalArgs args store) params') in
+                let newArgs = (assignArgs args params') in
                 runMain (List.append newArgs pstmts') store
+            )
+        )
+        | NewArrStmt (name, exp) -> (
+            write store name (ArrayVal (Array.create (extractNum (eval exp store)) 0) )
+        )
+        | ArrAssgStmt (name, exp1, exp2) -> (
+            match get store name with
+            | ArrayVal a -> (
+                let idx = extractNum (eval exp1 store) in
+                let ele = extractNum (eval exp2 store) in
+                Array.set a idx ele;
+                store
             )
         )
     in
